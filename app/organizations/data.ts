14e10d7bf00e3c8fa65e5e71380e518deb43c6cd
AvniDoshi <avni.doshi@outlook.com>
2026-08-13 16:30:21 -0700

    Clarify UC Irvine lab name

--- app/organizations/data.ts
+++ app/organizations/data.ts
@@ -11,7 +11,7 @@ export const organizations: Organization[] = [
   {
     slug: "uc-irvine-digital-learning-lab",
     name: "University of California, Irvine Digital Learning Lab",
-    shortName: "Digital Learning Lab",
+    shortName: "UC Irvine Digital Learning Lab",
     about: "UC Irvine's activity is led by the Digital Learning Lab (DLL), a research and development laboratory within the university's School of Education. The DLL was founded by Distinguished Professor Mark Warschauer to design, develop, and evaluate digital and AI-supported tools for promoting improved student learning and development, from preschool through graduate school. The DLL is the developer of PapyrusAI, the software platform used in the project.",
     role: "The Digital Learning Lab will serve as the main hub and coordinator of the WRITE AI Center, leading all activities, especially the further development and evaluation of PapyrusAI.",
     website: "https://www.digitallearninglab.org/",
